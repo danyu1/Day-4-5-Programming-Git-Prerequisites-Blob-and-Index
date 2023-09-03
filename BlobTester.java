b699288188bb4d842ef46a6dbff68d18a621edcd
danyul <danieltonyh06@gmail.com>
2023-09-03 14:36:23 -0700

    added add(String fileName) method to git.java
    -added mock text file that will be used to create the blob in git.tester

--- BlobTester.java
+++ BlobTester.java
@@ -4,7 +4,7 @@ import java.security.NoSuchAlgorithmException;
 public class BlobTester {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Blob blob1 = new Blob("test.txt");
-        blob1.writeToFile("This is some cool content.");
+        // blob1.writeToFile("This is some cool content.");
         blob1.createBlob(blob1.generateSHA1(blob1.convertToByteArray(blob1.getPath())), blob1.getPath());
     }
 }
--- Git.java
+++ Git.java
@@ -1,11 +1,16 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
 
 public class Git {
+    private int totalBlobs;
+
     public Git() throws IOException {
+        this.totalBlobs = 0;
         String objectsFolderName = "objects";
         // New file path
         Path objectFolderPath = Paths.get(objectsFolderName);
@@ -21,7 +26,22 @@ public class Git {
             file.createNewFile();
     }
 
-    public void add(String fileName) {
-
+    public void add(String fileName) throws NoSuchAlgorithmException, IOException {
+        Blob b = new Blob(fileName);
+        String SHA1 = b.generateSHA1(b.convertToByteArray(b.getPath()));
+        b.createBlob(SHA1, b.getPath());
+        if (totalBlobs == 0) {
+            PrintWriter pw = new PrintWriter("index");
+            pw.print(fileName + " : ");
+            pw.print(SHA1);
+            totalBlobs++;
+            pw.close();
+        } else {
+            PrintWriter pw = new PrintWriter("index");
+            pw.print("\n" + fileName + " : ");
+            pw.print(SHA1);
+            totalBlobs++;
+            pw.close();
+        }
     }
 }
