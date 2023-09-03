d55a19059e54bc6944eff1b88ec5b765720aa227
danyul <danieltonyh06@gmail.com>
2023-09-03 14:37:48 -0700

    updated tester
    -proves object folder receives sha1
    -index file get appropriate key, value pair

--- GitTester.java
+++ GitTester.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 public class GitTester {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Git init = new Git();
+        init.add("test.txt");
     }
 }
